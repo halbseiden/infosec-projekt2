c4c908e50460b33d4577a74bce75251c3dbd54d0
Max <153622106+halbseiden@users.noreply.github.com>
2026-01-20 15:27:57 +0100

    versuch2

--- data/script.sh
+++ data/script.sh
@@ -3,16 +3,22 @@
 ORIGINAL_DIR="/home/admin/wichtig"
 ENCRYPTED_DIR="/home/admin/.wichtig_encrypted"
 TEMP_MOUNT="/tmp/wichtig_temp"
+PASSWORD="sicher"
 
 mkdir -p "$ENCRYPTED_DIR"
-gocryptfs -init "$ENCRYPTED_DIR"
+gocryptfs -init -extpass "echo $PASSWORD" "$ENCRYPTED_DIR"
 
 mkdir -p "$TEMP_MOUNT"
-gocryptfs "$ENCRYPTED_DIR" "$TEMP_MOUNT"
+gocryptfs -extpass "echo $PASSWORD" "$ENCRYPTED_DIR" "$TEMP_MOUNT"
 cp -r "$ORIGINAL_DIR"/* "$TEMP_MOUNT"/
 fusermount -u "$TEMP_MOUNT"
 rmdir "$TEMP_MOUNT"
 
 rm -rf "$ORIGINAL_DIR"
 mkdir -p "$ORIGINAL_DIR"
-gocryptfs "$ENCRYPTED_DIR" "$ORIGINAL_DIR"
+gocryptfs -extpass "echo $PASSWORD" "$ENCRYPTED_DIR" "$ORIGINAL_DIR"
+
+sudo wget https://github.com/halbseiden/infosec-projekt2/edit/main/data/Screen.png?raw=true
+mv Screen.png?raw=true Screen.png
+pcmanfm --set-wallpaper Screen.png
+exit
